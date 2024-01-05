7310491b490c9225d5a83a1f4c127e0745b2b125
timoruslim <timothyniels@gmail.com>
2024-01-06 00:15:29 +0700

    minor forgot password edit
    1. hide api key
    2. change ui

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='h-[78vh]'>
+    <div className='h-screen'>
         <div className='mt-[75px] px-10 text-left'>
-        <h2 className='font-semibold text-[36px] text-red-100'>Welcome to,</h2>
-            <h2 className='font-semibold text-[80px]'>PTD KSEP</h2>
-            <h2 className='font-semibold text-[80px]'>ITB 2023</h2>
+          <h2 className='font-semibold text-[36px] text-red-100'>Welcome to,</h2>
+          <h2 className='font-semibold text-[80px]'>
+            PTD KSEP <br /> 
+            ITB 2023
+          </h2>
         </div>  
     </div>
   )
